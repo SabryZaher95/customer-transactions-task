37768d932f6411550667257c5afbd4cefe3ff81c
SabryZaher95 <sabryzaher6@gmail.com>
2024-07-16 00:07:16 +0300

    services

--- src/app/core/services/customers-transactions.service.ts
+++ src/app/core/services/customers-transactions.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CustomersTransactions } from '../interfaces/customers-transactions';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomersTransactionsService {
 
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
--- src/app/core/services/customers.service.ts
+++ src/app/core/services/customers.service.ts
@@ -1,16 +1,19 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Customers } from '../interfaces/customers';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomersService {
 
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    console.log(this.apiUrl)
+  }
 
   getCustomers(): Observable<Customers[]> {
     return this.http.get<Customers[]>(`${this.apiUrl}/customers`);
